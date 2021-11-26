a1157ff548cea22e0b272df10532d6f125d066c3
NgoQuocBao1010 <56999031+NgoQuocBao1010@users.noreply.github.com>
2021-11-26 13:44:59 +0700

    🆕 feat: bar-chart for all time reports

--- shipping-system/src/components/reports/BarChart.vue
+++ shipping-system/src/components/reports/BarChart.vue
@@ -3,7 +3,7 @@
         <apexcharts
             class="chart"
             type="bar"
-            :options="chartOptions"
+            :options="options"
             :series="series"
         ></apexcharts>
     </div>
@@ -22,36 +22,71 @@ export default {
     },
     data() {
         return {
-            series: this.data.series,
-        };
-    },
-    watch: {
-        data() {
-            this.generateChart();
-        },
-    },
-    methods: {
-        generateChart() {
-            this.chartOptions = {
-                ...this.chartOptions,
-                ...{
-                    xaxis: {
-                        categories: this.data.categories,
-                        labels: {
-                            style: {
-                                fontWeight: 700,
-                                cssClass: "apexcharts-xaxis-label",
-                            },
+            options: {
+                chart: {
+                    type: "bar",
+                    height: 50,
+                    fontFamily: "Quicksand, sans-serif",
+                },
+                plotOptions: {
+                    bar: {
+                        borderRadius: 4,
+                        horizontal: true,
+                        distributed: true,
+                    },
+                },
+                dataLabels: {
+                    enabled: true,
+                    distributed: true,
+                    style: {
+                        colors: ["#222"],
+                        fontWeight: 700,
+                    },
+                },
+                colors: ["#8fd6e1", "#7dccff", "#90ee90", "#ee4e4e"],
+                xaxis: {
+                    categories: ["Processing", "Delivering", "Delivered", "Failed"],
+                    labels: {
+                        style: {
+                            fontWeight: 700,
+                            fontSize: "13px",
                         },
                     },
                 },
-            };
-
-            this.series = this.data.series;
-        },
+                yaxis: {
+                    labels: {
+                        style: {
+                            fontWeight: 700,
+                            fontSize: "13px",
+                        },
+                    },
+                },
+                tooltip: {
+                    y: {
+                        formatter: (count) => {
+                            return `${count} orders`;
+                        },
+                        style: {
+                            fontWeight: "bold",
+                            cssClass: "apexcharts-xaxis-label",
+                        },
+                    },
+                },
+                legend: {
+                    position: "right",
+                    offsetX: 0,
+                    offsetY: 50,
+                    fontWeight: "700",
+                },
+            },
+            series: [{ name: "Orders", data: [] }],
+        };
     },
+    watch: {},
+    methods: {},
     created() {
-        this.generateChart();
+        const { processing, delivering, delivered, failed } = this.data;
+        this.series[0].data = [processing, delivering, delivered, failed];
     },
 };
 </script>
@@ -60,7 +95,7 @@ export default {
 .wrapper {
     width: 100%;
     min-width: 500px;
-    max-width: 900px;
+    max-width: 700px;
 
     button {
         background: #26e6a4;
